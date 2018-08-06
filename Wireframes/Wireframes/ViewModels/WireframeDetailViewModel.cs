22fbf9e2ac005b3c2140a393d9a770718ef675c8
Joey Richter <joerichterdev@gmail.com>
2018-08-05 17:36:00 -0700

    Implement Photo Capture/Selection

--- Wireframes/Wireframes/ViewModels/WireframeDetailViewModel.cs
+++ Wireframes/Wireframes/ViewModels/WireframeDetailViewModel.cs
@@ -4,9 +4,12 @@ using System.Windows.Input;
 using System;
 using Xamarin.Forms;
 using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System.IO;
 
 namespace Wireframes.ViewModels {
-    public class WireframeDetailViewModel {
+    public class WireframeDetailViewModel : BaseViewModel {
         public Wireframe Wireframe { get; private set; }
         public readonly IWireframeStore wireframeStore;
         public readonly IPageService pageService;
@@ -17,6 +20,8 @@ namespace Wireframes.ViewModels {
 
         // Commands
         public ICommand SaveWireframeCommand { get; private set; }
+        public ICommand CapturePictureCommand { get; private set; }
+        public ICommand SelectPictureCommand { get; private set; }
 
         public WireframeDetailViewModel(WireframeViewModel viewModel, IWireframeStore store, IPageService service) {
             if (viewModel == null) { throw new ArgumentNullException(nameof(viewModel)); }
@@ -25,6 +30,8 @@ namespace Wireframes.ViewModels {
             pageService = service;
 
             SaveWireframeCommand = new Command(async () => await SaveWireframe());
+            CapturePictureCommand = new Command(async () => await CapturePicture(viewModel));
+            SelectPictureCommand = new Command(async () => await SelectPicture());
 
             Wireframe = new Wireframe {
                 WireframeId = viewModel.WireframeId,
@@ -37,6 +44,7 @@ namespace Wireframes.ViewModels {
             };
         }
 
+        #region Commands
         private async Task SaveWireframe() {
             if (string.IsNullOrWhiteSpace(Wireframe.Title)) {
                 await pageService.DisplayAlert("Error", "Wireframe Title Cannot be Blank", "OK");
@@ -53,5 +61,60 @@ namespace Wireframes.ViewModels {
 
             await pageService.PopAsync();
         }
+
+        private async Task CapturePicture(WireframeViewModel wireframe) {
+            await CrossMedia.Current.Initialize();
+
+            if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported) {
+                DateTime currentDateTime = DateTime.Now;
+
+                StoreCameraMediaOptions options = new StoreCameraMediaOptions {
+                    Directory = "Captures",
+                    Name = $"{currentDateTime.ToString("yyyyMMddHHmmssff")}.jpg",
+                    PhotoSize = PhotoSize.Medium,
+                    CompressionQuality = 50
+                };
+
+                var capturedImage = await CrossMedia.Current.TakePhotoAsync(options);
+                if (capturedImage == null) return;
+
+                Wireframe.FileName = options.Name;
+                Wireframe.FileLocation = capturedImage.Path;
+                Wireframe.FileDate = currentDateTime;
+
+                OnPropertyChanged(nameof(Wireframe));
+            } else {
+                await App.Current.MainPage.DisplayAlert(
+                    "Cannot Take Photo", "This device does not support taking a photo", "OK");
+                return;
+            }
+        }
+
+        private async Task SelectPicture() {
+            await CrossMedia.Current.Initialize();
+
+            if (CrossMedia.Current.IsPickPhotoSupported) {
+                DateTime currentDateTime = DateTime.Now;
+
+                PickMediaOptions options = new PickMediaOptions {
+                    PhotoSize = PhotoSize.Medium,
+                    CompressionQuality = 50
+                };
+
+                var retrievedImage = await CrossMedia.Current.PickPhotoAsync();
+                if (retrievedImage == null) return;
+
+                Wireframe.FileName = Path.GetFileName(retrievedImage.Path);
+                Wireframe.FileLocation = retrievedImage.Path;
+                Wireframe.FileDate = currentDateTime;
+
+                OnPropertyChanged(nameof(Wireframe));
+            } else {
+                await App.Current.MainPage.DisplayAlert(
+                    "Cannot Select Photo", "This device does not support selecting a photo", "OK");
+                return;
+            }
+        }
+        #endregion
     }
 }
